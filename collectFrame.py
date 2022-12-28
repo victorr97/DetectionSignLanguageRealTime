2a3273b60d8e9662b5821b044041bc8932a78c4e
Victor <v.valles@students.salle.url.edu>
2022-12-28 19:31:37 +0100

    Al hacer Train guardo el modelo con pickle && restructuración de archivos

---
+++ collectFrame.py
@@ -0,0 +1,79 @@
+import mediapipe as mp
+import cv2
+import train
+import landmarks
+import csv
+
+total = []
+
+
+def cam_points(isCollectPhotos, class_name, trainData) -> None:
+    if isCollectPhotos:
+        mp_drawing = mp.solutions.drawing_utils
+        mp_holistic = mp.solutions.holistic
+        mp_drawing_styles = mp.solutions.drawing_styles
+
+        # WEBCAM
+        cap = cv2.VideoCapture(0)
+        if not cap.isOpened():
+            print("Failed to open camera")
+        with mp_holistic.Holistic(
+                min_detection_confidence=0.5,
+                min_tracking_confidence=0.5) as holistic:
+            while cap.isOpened():
+                success, image = cap.read()
+                if not success:
+                    print("Ignoring empty camera frame.")
+                    continue
+
+                image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+                image.flags.writeable = False
+                results = holistic.process(image)
+
+                # Draw landmark on the image
+                image.flags.writeable = True
+                image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+                mp_drawing.draw_landmarks(
+                    image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION,
+                    mp_drawing.DrawingSpec(color=(80, 109, 11), thickness=1, circle_radius=1),
+                    mp_drawing.DrawingSpec(color=(80, 256, 120), thickness=1, circle_radius=1))
+                mp_drawing.draw_landmarks(
+                    image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
+                    mp_drawing.DrawingSpec(color=(80, 109, 11), thickness=1, circle_radius=1),
+                    mp_drawing.DrawingSpec(color=(80, 256, 120), thickness=1, circle_radius=1))
+                mp_drawing.draw_landmarks(
+                    image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                    mp_drawing_styles.get_default_hand_landmarks_style(),
+                    mp_drawing_styles.get_default_hand_connections_style())
+                mp_drawing.draw_landmarks(
+                    image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                    mp_drawing_styles.get_default_hand_landmarks_style(),
+                    mp_drawing_styles.get_default_hand_connections_style())
+                cv2.imshow('MediaPipe Holistic Collect Photos', image)
+
+                # if ((results.face_landmarks is None) or (results.pose_landmarks is None) or (results.left_hand_landmarks is None) or (results.right_hand_landmarks is None)):
+                if (results.pose_landmarks is None) or (results.left_hand_landmarks is None):
+                    print("Doesn't detect all points of the person")
+                else:
+                    landmarks.formatLandmarks(results)
+                    row = landmarks.collectPointsRow(results, class_name)
+                    saveAllDataSignLanguage(cap, row)
+                # Cerrar CAM
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+        cap.release()
+        cv2.destroyAllWindows()
+    else:
+        train.trainingData(trainData)
+
+
+def saveAllDataSignLanguage(cap, row):
+    total.append(row)
+
+    if len(total) == 300:
+        for i in total:
+            with open('coords.csv', mode='a+', newline='') as file:
+                csv_writer = csv.writer(file)
+                csv_writer.writerow(i)
+                cap.release()
+                cv2.destroyAllWindows()
---
+++ landmarks.py
@@ -0,0 +1,40 @@
+import csv
+import os
+import numpy as np
+
+
+def formatLandmarks(puntos) -> None:
+    # numCoords = len(puntos.pose_landmarks.landmark) + len(puntos.face_landmarks.landmark) + len(puntos.left_hand_landmarks.landmark) + len(puntos.right_hand_landmarks.landmark)
+    numCoords = len(puntos.pose_landmarks.landmark) + len(puntos.left_hand_landmarks.landmark)
+    print("Detect " + str(numCoords) + " landmarks")
+
+    landmarks = ['class']
+    for val in range(1, numCoords + 1):
+        landmarks += ['x{}'.format(val), 'y{}'.format(val), 'z{}'.format(val), 'v{}'.format(val)]
+    if not os.path.isfile('coords.csv'):
+        with open('coords.csv', mode='w', newline='') as f:
+            mywriter = csv.writer(f)
+            mywriter.writerow(landmarks)
+
+
+def collectPointsRow(puntos, class_name):
+    # puntosCara = puntos.face_landmarks.landmark
+    # face_row = list(np.array([[landmark.x, landmark.y, landmark.z, landmark.visibility] for landmark in puntosCara]).flatten())
+
+    puntosPose = puntos.pose_landmarks.landmark
+    pose_row = list(
+        np.array([[landmark.x, landmark.y, landmark.z, landmark.visibility] for landmark in puntosPose]).flatten())
+
+    puntosManoIzq = puntos.left_hand_landmarks.landmark
+    manoIzq_row = list(
+        np.array([[landmark.x, landmark.y, landmark.z, landmark.visibility] for landmark in puntosManoIzq]).flatten())
+
+    # puntosManoDer = puntos.right_hand_landmarks.landmark
+    # manoDer_row = list(np.array([[landmark.x, landmark.y, landmark.z, landmark.visibility] for landmark in puntosManoDer]).flatten())
+
+    # row = face_row+pose_row+manoIzq_row+manoDer_row
+    row = pose_row + manoIzq_row
+
+    row.insert(0, class_name)
+
+    return row
