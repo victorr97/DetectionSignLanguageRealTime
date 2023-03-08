05200fb9620ecdbfbfbffc4a89dc6b62dde506ec
Victor <v.valles@students.salle.url.edu>
2023-03-08 17:06:09 +0100

    Renamed file buttons.js to buttons-popup.js

--- static/js/buttons-popup.js
+++ static/js/buttons-popup.js
@@ -1,22 +1,19 @@
-$( ".button_su_inner" ).mouseenter(function(e) {
+$(".button_su_inner").mouseenter(function(e) {
    var parentOffset = $(this).offset();
    var relX = e.pageX - parentOffset.left;
    var relY = e.pageY - parentOffset.top;
    $(this).prev(".su_button_circle").css({"left": relX, "top": relY });
    $(this).prev(".su_button_circle").removeClass("desplode-circle");
    $(this).prev(".su_button_circle").addClass("explode-circle");
-
-});
-
-$( ".button_su_inner" ).mouseleave(function(e) {
+}).mouseleave(function(e) {
      var parentOffset = $(this).offset();
      var relX = e.pageX - parentOffset.left;
      var relY = e.pageY - parentOffset.top;
      $(this).prev(".su_button_circle").css({"left": relX, "top": relY });
      $(this).prev(".su_button_circle").removeClass("explode-circle");
      $(this).prev(".su_button_circle").addClass("desplode-circle");
-
 });
+
 
 const clickBtn = document.getElementById("clickBtn");
 const popup = document.getElementById("popup");
