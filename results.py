b560f3e286d481d32db37faa2251b76b23f17684
Victor <v.valles@students.salle.url.edu>
2023-03-09 20:10:06 +0100

    clean code

--- results.py
+++ results.py
@@ -5,63 +5,6 @@ import cv2
 import pandas as pd
 import landmarks
 
-from fastapi import FastAPI
-from starlette.responses import HTMLResponse
-from starlette.websockets import WebSocket
-
-import base64
-
-
-def from_b64(uri):
-    '''
-        Convert from b64 uri to OpenCV image
-        Sample input: 'data:image/jpg;base64,/9j/4AAQSkZJR......'
-    '''
-    encoded_data = uri.split(',')[1]
-    data = base64.b64decode(encoded_data)
-    np_arr = np.fromstring(data, np.uint8)
-    img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
-    return img
-
-
-def to_b64(img):
-    '''
-        Convert from OpenCV image to b64 uri
-        Sample output: 'data:image/jpg;base64,/9j/4AAQSkZJR......'
-    '''
-    _, buffer = cv2.imencode('.jpg', img)
-    uri = base64.b64encode(buffer).decode('utf-8')
-    return f'data:image/jpg;base64,{uri}'
-
-
-def grayscale(data):
-    try:
-        image = from_b64(data)
-        # Do some OpenCV Processing
-        image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
-        image.flags.writeable = False
-
-        # Draw landmark on the image
-        image.flags.writeable = True
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-
-        # End for OpenCV Processing
-
-        return to_b64(image)
-    except:
-        # just in case some process is failed
-        # normally, for first connection
-        # return the original data
-        return data
-
-app = FastAPI()
-
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    while True:
-        data = await websocket.receive_text()
-        await websocket.send_text( grayscale(data) )
 
 def resultsWeb():
     print("*** RESULTS ***")
