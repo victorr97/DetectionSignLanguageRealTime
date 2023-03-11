e76362b758027bccd678a81493188209c9f273c7
Victor <v.valles@students.salle.url.edu>
2023-03-11 18:42:16 +0100

    Implemented all letter dropdown listeners and sent to the backend when you click on the letter not the send button.

--- static/js/buttons-popup.js
+++ static/js/buttons-popup.js
@@ -15,24 +15,6 @@ $(".button_su_inner").mouseenter(function(e) {
     $(this).prev(".su_button_circle").addClass("desplode-circle");
 });
 
-$(".three").mouseenter(function(e) {
-    let parentOffset = $(this).offset();
-    let relX = e.pageX - parentOffset.left;
-    let relY = e.pageY - parentOffset.top;
-    $(this).prev(".su_button_circle").css({"left": relX, "top": relY });
-    $(this).prev(".su_button_circle").removeClass("desplode-circle");
-    $(this).prev(".su_button_circle").addClass("explode-circle");
-}).mouseleave(function(e) {
-    let parentOffset = $(this).offset();
-    let relX = e.pageX - parentOffset.left;
-    let relY = e.pageY - parentOffset.top;
-    $(this).prev(".su_button_circle").css({"left": relX, "top": relY });
-    $(this).prev(".su_button_circle").removeClass("explode-circle");
-    $(this).prev(".su_button_circle").addClass("desplode-circle");
-});
-
-
-
 /************** GET ELEMENTS *********************/
 const clickBtnRecollect = document.getElementById("clickBtnRecollect");
 const clickBtnTrain = document.getElementById("clickBtnTrain");
--- static/js/recollectSign.js
+++ static/js/recollectSign.js
@@ -1,38 +1,56 @@
-/*// Obtener el elemento <select>
-const selectElement = document.getElementById("sign");
+document.addEventListener('DOMContentLoaded', function() {
+    let selectElement = document.querySelector('select');
 
-// Agregar un evento de escucha para el evento "change"
-selectElement.addEventListener("change", function() {
-    // Obtener el valor seleccionado del elemento <select>
-    const selectedValue = selectElement.value;
-
-    // Hacer algo con el valor seleccionado
-    console.log(selectedValue);
-});*/
-
-$(document).ready(function() {
-  //Click en submit
-  $('#enviar-btn').click(function(e) {
-    e.preventDefault(); // Prevenir que el formulario se envíe automáticamente
-
-    // Obtener el valor seleccionado del elemento <select>
-    const letterSign = $('#letterSign').val();
-
-    // Enviar una solicitud Fetch al backend
-    fetch('/procesar', {
-      method: 'POST',
-      body: JSON.stringify({ 'letterSign': letterSign }),
-      headers: {
-        'Content-Type': 'application/json; charset=utf-8'
-      }
-    })
-    .then(response => response.json())
-    .then(data => {
-      //Respuesta del servidor
-      console.log(data);
-    })
-    .catch(error => {
-      console.log(error);
+    //click on the item when the box is closed. (sign language letter)
+    selectElement.addEventListener('focus', () => {
+      selectElement.size = 5;
+      selectElement.classList.add('fadeIn');
+      selectElement.classList.remove('fadeOut');
+      selectElement.style.backgroundColor = '#FFF';
     });
-  });
+
+    //When you mouse over an item
+    selectElement.addEventListener('mouseover', () => {
+      if(selectElement.size === 1){
+         selectElement.style.backgroundColor = 'rgb(247, 247, 247)';
+      }
+    });
+
+    //When mousing out of an item
+    selectElement.addEventListener('mouseout', () => {
+      if(selectElement.size === 1){
+         selectElement.style.backgroundColor = '#FFF';
+      }
+    });
+
+    // When you click on an item after displaying it
+    selectElement.addEventListener('change', function(e) {
+        selectElement.size = 1;
+        selectElement.blur();
+        selectElement.classList.add('fadeOut');
+        selectElement.classList.remove('fadeIn');
+        selectElement.style.backgroundColor = '#FFF';
+
+        e.preventDefault(); // Prevent the form from being sent automatically
+
+        // User-selected letter
+        const letterSign = selectElement.value;
+
+        // Enviar una solicitud Fetch al backend
+        fetch('/procesar', {
+            method: 'POST',
+            body: JSON.stringify({ 'letterSign': letterSign }),
+            headers: {
+                'Content-Type': 'application/json; charset=utf-8'
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            //Respuesta del servidor
+            console.log(data);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    });
 });
