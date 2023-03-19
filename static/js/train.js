6913095e43aa8eba728b8f4d3e13df5d988e55bf
Victor <v.valles@students.salle.url.edu>
2023-03-19 20:24:27 +0100

    Implemented all image listeners with their CSS of selected and unselected.

--- static/js/train.js
+++ static/js/train.js
@@ -1,36 +1,45 @@
-/*
-const scrollButton = document.querySelector(".scroll-down");
-const scrollContent = document.querySelector(".scroll-content");
-
-
-let scrollPosition = 0;
-
-scrollButton.addEventListener("click", () => {
-  scrollPosition += 400;
-  scrollContent.style.transform = `translateY(-${scrollPosition}px)`;
-});
-
- */
-
 const rightDiv = document.getElementById("containerImg");
 const scrollUpBtn = document.getElementById('scroll-up');
 const scrollDownBtn = document.getElementById('scroll-down');
+const imgsClick = document.querySelectorAll('a');
 
 let scrollPosition = 0;
+const height3Img = 765;
+
+// Agrego un Event Listener click para cada elemento <a> (imagenes)
+imgsClick.forEach(function (element) {
+    element.addEventListener('click', function (event) {
+        const allImg = document.querySelectorAll('img');
+        //Para todas las imagenes si contiene la clase activeImg activada significa que estaba anteriormente clicada y se ha de substituir
+        allImg.forEach(function (imagen) {
+            if (imagen.classList.contains("activeImg")){
+                imagen.classList.replace('activeImg', 'notActiveImg');
+            }
+        });
+        //Añado la clase de activeImg a la img donde el evento se ha clicado
+        const img = event.target
+        if (img.classList.contains("notActiveImg")){
+            img.classList.replace('notActiveImg', 'activeImg')
+            console.log(event.target.alt)
+        }
+    });
+});
 
 function handleScrollUp() {
-    scrollPosition -= 1500;
+    scrollPosition -= height3Img;
     scrollSmoothly();
 }
 
 function handleScrollDown() {
-    scrollPosition += 1500;
+    scrollPosition += height3Img;
     scrollSmoothly();
 }
-
+/*
+* La función scrollSmoothly calcula la distancia entre la posición actual y la posición deseada y hace un efecto de scrolling.
+*/
 function scrollSmoothly() {
     const targetPosition = scrollPosition;
-    const duration = 500; // Duración de la animación en milisegundos
+    const duration = 500; //500ms
     const startPosition = rightDiv.scrollTop;
     const distance = targetPosition - startPosition;
     let startTime = null;
@@ -40,31 +49,27 @@ function scrollSmoothly() {
             startTime = currentTime;
         }
         const elapsedTime = currentTime - startTime;
-        const position = easeInOutQuad(elapsedTime, startPosition, distance, duration);
-        rightDiv.scrollTop = position;
+        rightDiv.scrollTop = scrolling(elapsedTime, startPosition, distance, duration);
         if (elapsedTime < duration) {
+            // Utilizo requestAnimationFrame para animar gradualmente la posición de desplazamiento.
             requestAnimationFrame(animation);
         }
     }
-
-    function easeInOutQuad(currentTime, startValue, changeInValue, duration) {
-      currentTime /= duration / 2;
-      if (currentTime < 1) {
-        return (changeInValue / 2) * currentTime * currentTime + startValue;
-      }
-      currentTime--;
-      return (-changeInValue / 2) * (currentTime * (currentTime - 2) - 1) + startValue;
+    //La función scrolling crea un efecto de scroll según la distancia, la posicion y los ms del efecto.
+    function scrolling(elapsedTime, startPosition, distance, duration) {
+        elapsedTime /= duration / 2;
+        if (elapsedTime < 1) {
+            return (distance / 2) * elapsedTime * elapsedTime + startPosition;
+        }
+        elapsedTime--;
+        return (-distance / 2) * (elapsedTime * (elapsedTime - 2) - 1) + startPosition;
     }
 
     requestAnimationFrame(animation);
 }
 
 /*
-* En esta versión actualizada, la función handleScrollUp y handleScrollDown ahora simplemente actualizan la posición de desplazamiento en 100, y luego llaman a la nueva función scrollSmoothly.
 
-La función scrollSmoothly calcula la distancia entre la posición actual y la posición deseada, y luego utiliza requestAnimationFrame para animar gradualmente la posición de desplazamiento. La animación utiliza una función de aceleración (ease) para crear un efecto suave y natural.
-
-Con estos cambios, los usuarios verán un efecto de desplazamiento suave cuando hagan clic en los botones de subir y bajar, y el desplazamiento se detendrá automáticamente cuando se alcance la posición deseada. Puedes ajustar la cantidad y duración del desplazamiento cambiando los valores en la función scrollSmoothly.
 *
 * */
 
