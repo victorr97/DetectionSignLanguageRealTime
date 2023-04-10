b3854472429d862809edf646a510fe0ea695698b
Victor <v.valles@students.salle.url.edu>
2023-04-10 20:04:42 +0200

    Pop up finished at the start of the game

--- static/js/game.js
+++ static/js/game.js
@@ -6,54 +6,79 @@ const img = document.querySelector('.frameWebCam');
 const loading = document.getElementById("loading");
 let scrollPosition = 0;
 const height3Img = 765;
+let activeIndex = 0;
 
 img.addEventListener('load', function () {
     loading.style.display = "none";
     textSelectImg.style.display = "flex";
+    firstLetter("A")
     countDownStartGame()
 });
 
+function firstLetter(letter) {
+    console.log(imgsClick[activeIndex])
+    imgsClick[activeIndex].classList.replace('notActiveImg', 'activeImg');
+    if (imgsClick[activeIndex].classList.contains("notActiveImg")) {
+        imgsClick[activeIndex].classList.replace('notActiveImg', 'activeImg');
+        console.log("changed")
+        console.log(imgsClick[activeIndex])
+    }
+
+    textSelectImg.innerHTML = "SIGNO PARA REALIZAR: <strong>" + letter + "</strong>";
+}
+
+function updateActiveImage() {
+
+    // Cambiamos la clase del elemento activo actual a "notActiveImg"
+    imgsClick[activeIndex].classList.replace('activeImg', 'notActiveImg');
+
+    // Incrementamos el índice del elemento activo
+    activeIndex++;
+
+    // Cambiamos la clase del nuevo elemento activo a "activeImg"
+    imgsClick[activeIndex].classList.replace('notActiveImg', 'activeImg');
+}
 
 function countDownStartGame() {
     Swal.fire({
-      title: 'Preparados para jugar',
-      html: '<div><h3 id="countdown" style="font-size: 60px; font-weight: bold;">5</h3></div>',
-      timer: 5000,
-      timerProgressBar: true,
-      didOpen: () => {
-        Swal.showLoading();
-        const countdown = document.getElementById('countdown');
-        let counter = 5;
-        const timer = setInterval(() => {
-          counter--;
-          countdown.textContent = counter;
-          if (counter === 0) {
-            clearInterval(timer);
-            countdown.classList.add('highlight');
-            Swal.hideLoading();
-          }
-        }, 1000);
-      },
-      allowOutsideClick: false,
-      allowEscapeKey: false,
-      allowEnterKey: false,
-      backdrop: false,
-      showConfirmButton: false
-    }).then((result) => {
-      // Acción a realizar cuando se cierre el SweetAlert (no se ejecutará en este caso)
-    });
-
-    // Agregamos un estilo CSS para resaltar el número al final de la cuenta atrás
-    Swal.insertRule('.highlight { background-color: yellow; }', true);
-
-    // Evento que se dispara cuando termina la cuenta atrás
-    Swal.on('timer', () => {
-      // Acción a realizar cuando termina la cuenta atrás
+        title: 'Prepárate para jugar en',
+        html: '<div><h3 id="countdown" style="font-size: 60px; font-weight: bold; color: red;">5</h3></div>',
+        timer: 6000,
+        timerProgressBar: true,
+        didOpen: () => {
+            Swal.showLoading();
+            const countdown = document.getElementById('countdown');
+            let counter = 5;
+            const timer = setInterval(() => {
+                counter--;
+                if (counter === 0) {
+                    countdown.textContent = 'YA!';
+                    countdown.classList.add('highlight');
+                    setTimeout(() => {
+                        countdown.textContent = '';
+                        countdown.classList.remove('highlight');
+                        clearInterval(timer);
+                        Swal.hideLoading();
+                        Swal.close();
+                    }, 1000);
+                    clearInterval(timer);
+                } else {
+                    countdown.textContent = counter;
+                }
+            }, 1000);
+        },
+        allowOutsideClick: false,
+        allowEscapeKey: false,
+        allowEnterKey: false,
+        backdrop: false,
+        showConfirmButton: false
+    }).then(() => {
+        console.log("EMPIEZA EL JUEGO")
     });
 }
 
 
-function resetLetter(){
+function resetLetter() {
     textSelectImg.innerHTML = "SIGNO: ";
     allImg.forEach(function (imagen) {
         if (imagen.classList.contains("activeImg")) {
