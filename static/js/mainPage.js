54b847df5529154d6d26f0c4a5c9fcc5d388e7ce
Victor <v.valles@students.salle.url.edu>
2023-04-13 19:24:45 +0200

    fixed bug of green button animation when other animation is not finished yet

--- static/js/mainPage.js
+++ static/js/mainPage.js
@@ -1,20 +1,24 @@
 /************** EFFECT 3 BUTTONS MAINPAGE *********************/
-$(".button_su_inner").mouseenter(function(e) {
-    let parentOffset = $(this).offset();
-    let relX = e.pageX - parentOffset.left;
-    let relY = e.pageY - parentOffset.top;
-    $(this).prev(".su_button_circle").css({"left": relX, "top": relY });
-    $(this).prev(".su_button_circle").removeClass("desplode-circle");
-    $(this).prev(".su_button_circle").addClass("explode-circle");
-}).mouseleave(function(e) {
-    let parentOffset = $(this).offset();
-    let relX = e.pageX - parentOffset.left;
-    let relY = e.pageY - parentOffset.top;
-    $(this).prev(".su_button_circle").css({"left": relX, "top": relY });
-    $(this).prev(".su_button_circle").removeClass("explode-circle");
-    $(this).prev(".su_button_circle").addClass("desplode-circle");
-});
 
+$(".button_su_inner").on("animationend", function () {
+    $(this).off("animationend"); // remove the event listener to prevent multiple firings
+    // Add your JavaScript code here to trigger after animation
+    $(".button_su_inner").mouseenter(function (e) {
+        let parentOffset = $(this).offset();
+        let relX = e.pageX - parentOffset.left;
+        let relY = e.pageY - parentOffset.top;
+        $(this).prev(".su_button_circle").css({"left": relX, "top": relY});
+        $(this).prev(".su_button_circle").removeClass("desplode-circle");
+        $(this).prev(".su_button_circle").addClass("explode-circle");
+    }).mouseleave(function (e) {
+        let parentOffset = $(this).offset();
+        let relX = e.pageX - parentOffset.left;
+        let relY = e.pageY - parentOffset.top;
+        $(this).prev(".su_button_circle").css({"left": relX, "top": relY});
+        $(this).prev(".su_button_circle").removeClass("explode-circle");
+        $(this).prev(".su_button_circle").addClass("desplode-circle");
+    });
+});
 
 /************** GET ELEMENTS *********************/
 const clickBtnRecollect = document.getElementById("clickBtnRecollect");
